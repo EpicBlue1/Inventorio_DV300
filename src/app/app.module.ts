7c8dddef9fa122c5a5fbdac252bd04ae91db713c
EpicBlue1 <21100115@virtualwindow.co.za>
2023-03-04 18:48:33 +0200

    Sidenav added some reusable components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
-import { InputComponent } from './input/input.component';
+import { NavComponent } from './components/partials/nav/nav.component';
+import { ButtonComponent } from './components/common/button/button.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ButtonComponent,
-    InputComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavComponent, ButtonComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
